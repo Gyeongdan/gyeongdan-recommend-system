936137df363eda54105a89a2070f9bc38fae96af
mandu <jys0972@gmail.com>
2024-07-09 18:00:28 +0900

    Create user_classification.py : 유저의 관심사를 enum으로 관리

---
+++ user_classification.py
@@ -0,0 +1,8 @@
+from enum import Enum
+
+class ClassificationItem(Enum):
+    ISSUE_FINDER = 'issue finder'
+    LIFESTYLE_CONSUMER = 'lifestyle consumer'
+    ENTERTAINER = 'entertainer'
+    TECH_SPECIALIST = 'tech specialist'
+    PROFESSIONALS = 'professionals'
